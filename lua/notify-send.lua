7075975b4a013fd9fad4744f076fea7ef966ff3a
Trevor Facer <trevorf@ifit.com>
2022-08-10 10:08:16 -0600

    feat(schedule_line): Add ability to schedule line
    This takes the text from the current line, then prompts the user for a
    time, and schedules a reminder with that text at the given time.

--- lua/notify-send.lua
+++ lua/notify-send.lua
@@ -1,10 +1,26 @@
 local notify_send = {}
+local api = vim.api
 
 local setup = function(opts)
   notify_send.notify_binary = opts.notify_binary and opts.notify_binary or "notify-send"
   return notify_send
 end
 notify_send.setup = setup
+
+local schedule_line = function()
+  local win_id = api.nvim_get_current_win()
+  local bufnr = api.nvim_win_get_buf(win_id)
+  local cursor_position = api.nvim_win_get_cursor(win_id)
+  local row = cursor_position[1]
+  local current_line_text_table = api.nvim_buf_get_text(bufnr, row - 1, 0, row - 1, -1, {})
+  local current_line_text = current_line_text_table[1]
+  local when = vim.fn.input("Remind '" .. current_line_text .. "' when? ")
+  local command = string.format("echo '%s \"%s\" \"%s\" &>/tmp/notify-send-lua-remind-out.txt' | at %s &>/dev/null",
+    notify_send.notify_binary, "Reminder", current_line_text, when)
+  print(command)
+  os.execute(command)
+end
+notify_send.schedule_line = schedule_line
 
 local remind = function()
   local title = vim.fn.input("Reminder title? ")
--- plugin/notify-send.lua
+++ plugin/notify-send.lua
@@ -5,3 +5,6 @@ local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<leader><leader>R", notify_send.remind, opts)
 vim.keymap.set("n", "<leader><leader>S", notify_send.schedule, opts)
+vim.keymap.set("n", "<leader>Sr", notify_send.remind, opts)
+vim.keymap.set("n", "<leader>Sj", notify_send.schedule, opts)
+vim.keymap.set("n", "<leader>Sl", notify_send.schedule_line, opts)
